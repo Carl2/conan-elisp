8470a0fe4be91fe9c5dc1433dc9c2ff566d137cc
Calle <olsen.carl@gmail.com>
2023-03-27 23:28:03 +0200

    Added package header
    Needs some more work

--- elisp-conan.el
+++ elisp-conan.el
@@ -1,3 +1,40 @@
+;;; conan-elisp.el --- Generate header for c++ -*- lexical-binding: t -*-
+
+;; Author: Carl Olsen
+;; Maintainer: Calle
+;; Version: 0.0.1
+;; Package-Requires: (dependencies)
+;; Homepage: https://cocode.se
+;; Keywords: conan
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; todo
+
+;;; Code:
+
+
+(provide 'conan-elisp)
+
+;;; conan-elisp.el ends here
+
 (defun col/generic-conan-heading (heading)
   "docstring"
   (lexical-let ((heading heading))
